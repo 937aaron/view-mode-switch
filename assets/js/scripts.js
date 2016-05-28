67b41058517e032fbc62efaaaabc5b55f96d96ae
Aaron <newcomer.aaron@icloud.com>
2016-05-28 13:45:16 -0400

    Add JS function

---
+++ assets/js/scripts.js
@@ -0,0 +1,18 @@
+var listButton = document.querySelector(".listbutton");
+var gridButton = document.querySelector(".gridbutton");
+
+gridButton.addEventListener("click", toggleGrid);
+listButton.addEventListener("click", toggleList);
+
+
+
+function toggleGrid (){
+    document.getElementById("activeSheet").href="assets/css/gridstyles.css";
+    console.log("Hello");
+};
+function toggleList(){
+  document.getElementById("activeSheet").href="assets/css/liststyles.css";
+  console.log("goodbye");
+};
+var x = 1;
+console.log(x);
